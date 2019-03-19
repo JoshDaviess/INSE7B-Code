762366c77ddfbfe6ce10c3ecc9915fee9c06a7c3
Gary Leung <UP814734@uni.ds.port.ac.uk>
2019-03-19 17:26:45 +0000

    DB testing

--- server.js
+++ server.js
@@ -3,11 +3,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mysql = require('mysql2/promise');
 
 app.use(express.static(path.join(__dirname + '/webpages')));
 
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'database'
+  }).then(function(conn){
+    var result = conn.query('select * from Shop');
+    conn.end();
+    console.log(result);
+    res.send(result);
+  });
 });
 
 // starts the server
