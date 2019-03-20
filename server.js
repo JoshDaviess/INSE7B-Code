2a4dd1555339fec5f3c7c28ee9763815b5497c2e
Gary Leung <garyleunguk@outlook.com>
2019-03-20 14:26:10 +0000

    changes reverted

--- server.js
+++ server.js
@@ -3,16 +3,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-//const mysql = require('mysql');
+const mysql = require('mysql');
 
 app.use(express.static(path.join(__dirname + '/webpages')));
 
-//var sqlconnect =  mysql.createConnection({
-//      host: 'localhost',
-//      user: 'root',
-//      password: 'root',
-//      database: 'inseDB'
-//  });
+var sqlconnect =  mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'inseDB'
+  });
 
 // starts the server
 app.listen(8080, (err) => {
@@ -20,8 +20,8 @@ app.listen(8080, (err) => {
   else console.log('Server started on port 8080');
 });
 
-//sqlconnect.connect();
-//sqlconnect.query('Select * from Shop', function (error, results, fields) {
-//  if (error) throw error;
-//  console.log(results);
-//});
+sqlconnect.connect();
+sqlconnect.query('Select * from Shop', function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
