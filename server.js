dea2a2657f70f6609aef7210adcccb48a5b14e75
JoshDaviess <44232357+JoshDaviess@users.noreply.github.com>
2019-03-20 17:55:53 +0000

    Revert "updated"
    This reverts commit 252417eeebb1fd46b53d2d8639ba0e5b987128fd.

--- server.js
+++ server.js
@@ -13,14 +13,15 @@ var sqlconnect =  mysql.createConnection({
       password: 'root',
       database: 'inseDB'
   });
+
 // starts the server
 app.listen(8080, (err) => {
   if (err) console.error('Error starting server', err);
   else console.log('Server started on port 8080');
 });
 
-sqlconnect.connect();
-sqlconnect.query('Select * from Shop', function (error, results, fields) {
-  if (error) throw error;
-  console.log(results);
-});
+//sqlconnect.connect();
+//sqlconnect.query('Select * from Shop', function (error, results, fields) {
+//  if (error) throw error;
+//  console.log(results);
+//});
