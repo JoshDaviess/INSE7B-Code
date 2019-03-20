ac40d3c53af823273032fa6ec788ea2518f6eb11
JoshDaviess <44232357+JoshDaviess@users.noreply.github.com>
2019-03-20 17:55:50 +0000

    Revert "updated"
    This reverts commit ce85b3b85f3fd1e78a9504b75244744a0be4a916.

--- server.js
+++ server.js
@@ -3,25 +3,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-//const mysql = require('mysql');
+const mysql = require('mysql');
 
-//app.use(express.static(path.join(__dirname + '/webpages')));
+app.use(express.static(path.join(__dirname + '/webpages')));
 
-//var sqlconnect =  mysql.createConnection({
-//      host: 'localhost',
-//      user: 'root',
-//      password: 'root',
-//      database: 'inseDB'
-//  });
-
+var sqlconnect =  mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'inseDB'
+  });
 // starts the server
 app.listen(8080, (err) => {
   if (err) console.error('Error starting server', err);
   else console.log('Server started on port 8080');
 });
 
-//sqlconnect.connect();
-//sqlconnect.query('Select * from Shop', function (error, results, fields) {
-//  if (error) throw error;
-//  console.log(results);
-//});
+sqlconnect.connect();
+sqlconnect.query('Select * from Shop', function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
