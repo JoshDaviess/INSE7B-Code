bf462483a8333bdca43fa692bb6fbb3417b8ef02
Josh <up812658@myport.ac.uk>
2019-03-26 12:29:31 +0000

    API for retreiving barber details based on the barberID

--- server.js
+++ server.js
@@ -33,6 +33,13 @@ app.get('/latitude', function (req, res) {
           res.end(JSON.stringify(recordset)); // Result in JSON format
       });
   });
+
+  app.get('/barber/:barbid', function (req, res) {
+          sqlconnect.query('SELECT * FROM inseDB.Shop where shopID = ?', req.params.barbid, function(err, recordset) {
+            if(err) console.log(err);
+            res.end(JSON.stringify(recordset)); // Result in JSON format
+        });
+    });
 // var result = [];
 // var  getLatitudeFromDB = function(callback) {
 //
