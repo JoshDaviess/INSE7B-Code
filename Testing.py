38ca63e3ccd7fac23306d3d0cd94b81f8865f6c8
JoshDaviess <44232357+JoshDaviess@users.noreply.github.com>
2019-04-04 17:57:18 +0100

    More team tests, and minor changes to javascript

--- Testing.py
+++ Testing.py
@@ -1,10 +1,12 @@
 from helium.api import *
+import time
 
 def main():
     address = input('Please enter the address of the website: ')
     print("beginning testing")
     homepage(address)
-    barbIncrease()
+    barbIncrease(address)
+    barbDecrease(address)
     
 
 def homepage(address):
@@ -20,6 +22,31 @@ def barbIncrease(address):
     start_chrome()
     go_to(address + ":8080")
     click("BarberMode")
-    click(Button("IncreaseB"))
-    print("Homepage Running, test passed")
+    click("IncreaseB")
+    time.sleep(2)
+    print("Value increased, Begining extreme value test")
+    for i in range(10):
+        click("IncreaseB")
+        time.sleep(0.3)
+    time.sleep(2)
+    print("Barber Increase Running, test passed")
+    kill_browser()  
+    
+def barbDecrease(address):
+    print("beginning barber Decrease test")
+    start_chrome()
+    go_to(address + ":8080")
+    click("BarberMode")
+    print("Increasing Value for test")
+    for i in range(10):
+        click("IncreaseB")
+        time.sleep(0.5)
+    time.sleep(2)
+    print("Barber Increased, begining decrease")
+    click("DecreaseB")
+    print("Barber decreased. Beginning extreme decrease")
+    for i in range(100):
+        click("DecreaseB")
+        time.sleep(0.1)
+    print("Extreme decrease test passed")
     kill_browser()   
--- webpages/increaseDecrease.js
+++ webpages/increaseDecrease.js
@@ -2,23 +2,35 @@ function incrementBarb(){
     var value = parseInt(document.getElementById('BarbNumber').value, 10);
     value = isNaN(value) ? 0 : value;
     value++;
+    if(value>=6){
+      value = 6
+    }
     document.getElementById('BarbNumber').value = value;
 }
 function decreaseBarb(){
     var value = parseInt(document.getElementById('BarbNumber').value, 10);
     value = isNaN(value) ? 0 : value;
     value--;
+    if(value<=0){
+      value = 0
+    }
     document.getElementById('BarbNumber').value = value;
 }
 function increaseCust(){
     var value = parseInt(document.getElementById('CustNumber').value, 10);
     value = isNaN(value) ? 0 : value;
     value++;
+    if(value>=20){
+      value = 20
+    }
     document.getElementById('CustNumber').value = value;
 }
 function decreaseCust(){
     var value = parseInt(document.getElementById('CustNumber').value, 10);
     value = isNaN(value) ? 0 : value;
     value--;
+    if(value<=0){
+      value = 0
+    }
     document.getElementById('CustNumber').value = value;
 }
